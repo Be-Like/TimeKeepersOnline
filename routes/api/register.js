592db7e3cc09514fd1f4c7f7a388cd6fffb14229
Jacob Simpson <jsimpson@email.sc.edu>
2019-10-30 15:46:04 -0400

    Backend register new user is complete.

--- routes/api/register.js
+++ routes/api/register.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -14,9 +17,58 @@ router.post(
       .not()
       .isEmpty(),
     check('email', 'Invalid email').isEmail(),
-    check('Password', 'Please enter a password').isLength({ min: 1 }) // TODO: create better password validation upon completion
+    check('password', 'Please enter a password').isLength({ min: 1 }) // TODO: create better password validation upon completion
   ],
-  async (req, res) => {}
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Validate that user does not already exist
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // TODO: set default avatar
+
+      // Create new user
+      user = new User({
+        name,
+        email,
+        avatar: null,
+        password
+      });
+
+      // Encrypt the password using bcrypt
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // Save user
+      await user.save();
+
+      // Create jwt and pass user id
+      jwt.sign(
+        { user: { id: user.id } },
+        config.get('jwtSecret'),
+        (error, token) => {
+          if (error) {
+            throw error;
+          }
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server error');
+    }
+  }
 );
 
 module.exports = router;
