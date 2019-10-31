3b3a7393071792dfbd5cd838f5294f76ff96ac41
Jake Simpson <simpson.jacob.barry@gmail.com>
2019-10-31 08:58:29 -0400

    Backend: user can now update password

--- routes/api/register.js
+++ routes/api/register.js
@@ -75,5 +75,34 @@ router.post(
 // @desc Update user
 // @access Private
 // TODO: work on updating user information.
+router.put(
+  '/',
+  [check('password', 'Please enter a password').isLength({ min: 1 })],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { password } = req.body;
+
+    try {
+      // Get the user
+      const user = await User.findOne({ user: req.id });
+
+      // Update users password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      // save updated user
+      await user.save();
+
+      res.json(user);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
