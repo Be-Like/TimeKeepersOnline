a89a4920b4e8ac3f06ba682c55da39bb1cf47b9c
Jake Simpson <simpson.jacob.barry@gmail.com>
2019-11-27 10:39:42 -0500

    Finished the Calendar component for the CalendarPage.

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,46 +1,111 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
-import '../../stylesheet/calendar/calendar.css';
+import Moment from 'react-moment';
 import moment from 'moment';
 
-const Calendar = props => {
-  const [currentMonth, setCurrentMonth] = useState(new Date());
-  const [selectedDate, setSelectedDate] = useState(new Date());
+import '../../stylesheet/calendar/calendar.css';
+
+const Calendar = ({ date }) => {
+  const [currentMonth, setCurrentMonth] = useState(new moment());
+  const [selectedDate, setSelectedDate] = useState(new moment());
 
   const nextMonth = () => {
-    setCurrentMonth(moment.addMonths(currentMonth, 1));
+    setCurrentMonth(moment(currentMonth).add(1, 'month'));
   };
   const prevMonth = () => {
-    setCurrentMonth(moment.subMonths(currentMonth, 1));
+    setCurrentMonth(moment(currentMonth).subtract(1, 'month'));
   };
 
-  const renderHeader = () => {
-    const dateFormat = 'MMMM YYYY';
+  const onDateClick = day => {
+    setSelectedDate(day);
+    date(day);
+    console.log(day);
+  };
+
+  const RenderHeader = () => {
+    const dateFormat = 'MMM YYYY';
 
     return (
       <div className='header row flex-middle'>
         <div className='col col-start'>
-          <div className='icon' onClick={this.prevMonth}>
+          <div className='icon' onClick={prevMonth}>
             chevron_left
           </div>
         </div>
         <div className='col col-center'>
-          <span>{moment.format(currentMonth, dateFormat)}</span>
+          <Moment format={dateFormat}>{currentMonth}</Moment>
         </div>
-        <div className='col col-end' onClick={this.nextMonth}>
+        <div className='col col-end' onClick={nextMonth}>
           <div className='icon'>chevron_right</div>
         </div>
       </div>
     );
   };
 
+  const RenderDays = () => {
+    const days = moment.weekdaysShort();
+
+    const dayList = days.map(day => (
+      <div className='col col-center' key={day}>
+        {day}
+      </div>
+    ));
+
+    return <div className='days row'>{dayList}</div>;
+  };
+
+  const RenderCells = () => {
+    const monthStart = moment(currentMonth).startOf('month');
+    const monthEnd = moment(monthStart).endOf('month');
+    const startDate = moment(monthStart).startOf('week');
+    const endDate = moment(monthEnd).endOf('week');
+
+    const dateFormat = 'D';
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = '';
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = moment(day).format(dateFormat);
+        const cloneDay = day;
+
+        days.push(
+          <div
+            className={`col cell ${
+              moment(day).isBefore(monthStart) || moment(day).isAfter(monthEnd)
+                ? 'disabled'
+                : moment(day).isSame(selectedDate)
+                ? 'selected'
+                : ''
+            }`}
+            key={day}
+            onClick={() => onDateClick(moment(cloneDay))}
+          >
+            <span className='number'>{formattedDate}</span>
+            <span className='bg'>{formattedDate}</span>
+          </div>
+        );
+        day = moment(day).add(1, 'day');
+      }
+      rows.push(
+        <div className='row' key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+
+    return <div className='body'>{rows}</div>;
+  };
+
   return (
     <div className='calendar'>
-      <div>Header</div>
-      {renderHeader}
-      <div>Days</div>
-      <div>Cells</div>
+      <RenderHeader />
+      <RenderDays />
+      <RenderCells />
     </div>
   );
 };
--- client/src/components/calendar/CalendarPage.js
+++ client/src/components/calendar/CalendarPage.js
@@ -1,19 +1,30 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import moment from 'moment';
+import Moment from 'react-moment';
 
 import Calendar from './Calendar';
+import '../../stylesheet/calendar/calendarPage.css';
 
 const CalendarPage = props => {
+  const [dateSelected, setDateSelected] = useState(new moment());
+
+  const selectedDate = date => {
+    setDateSelected(date);
+  };
+
   return (
     <Fragment>
       <div className='container'>
         <div className='row'>
           <div className='col-6 calendar-calendar-height'>
-            <h4>Hello Calendar</h4>
+            <Calendar date={selectedDate} />
           </div>
           <div className='col-6 calendar-calendar-height'>
-            <Calendar />
+            <h4>
+              Hello <Moment date={dateSelected} />
+            </h4>
           </div>
         </div>
       </div>
